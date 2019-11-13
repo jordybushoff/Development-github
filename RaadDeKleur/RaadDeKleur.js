0b6b06a29e21ab23e03d70f89ce87f29ecd24c27
jordybushoff <47558264+jordybushoff@users.noreply.github.com>
2019-11-13 12:27:09 +0100

    Memory game
    De kaarten tevoorschijn halen.

--- RaadDeKleur/RaadDeKleur.js
+++ RaadDeKleur/RaadDeKleur.js
@@ -1,3 +1,12 @@
+var buttons = document.getElementsByClassName('colourButton');
+
+
 var heading;
 heading = document.getElementById('colourValue');
 heading.innerHTML = 'Hello world!';
+
+function setButtonColour(button, red, green, blue) {
+    button.setAttribute('style', 'background-color: rgb(' + red + ',' + green + ',' + blue + ');');
+	
+	setButtonColour(buttons[0], 0, 0, 255);
+}
