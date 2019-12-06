0539a8797b91e0755d404b034b217f55d35b1778
jordybushoff <47558264+jordybushoff@users.noreply.github.com>
2019-12-07 00:30:30 +0100

    proberen kleur

--- RaadDeKleur/RaadDeKleur.js
+++ RaadDeKleur/RaadDeKleur.js
@@ -1,12 +1,15 @@
-
-var buttons = document.getElementsByClassName('colourButton');
+var buttons = document.getElementsByClassName('colourButton')[0];
 
 
 var heading;
 heading = document.getElementById('colourValue');
 heading.innerHTML = 'Hello world!';
 
+'use strict';
 function setButtonColour(button, red, green, blue) {
-    "use strict";
-    button.setAttribute('style', 'background-color: rgb(' + red + ',' + green + ',' + blue + ');');
+    button.setAttribute('style',
+                        'background-color: rgb(' + red + ',' + green + ',' + blue + ');'
+                       );
+    
 }
+setButtonColour(buttons[0], 0, 0, 255);
