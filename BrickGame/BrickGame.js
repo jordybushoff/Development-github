bd73e293f75b661c7bb2e72cbf29686ebcb12a76
jordybushoff <47558264+jordybushoff@users.noreply.github.com>
2019-12-07 19:57:41 +0100

    Update BrickGame.js
    gewerkt aan de bricks van de game. De collision werkt nog niet maar heb ze wel in het canvas geplaatst.

--- BrickGame/BrickGame.js
+++ BrickGame/BrickGame.js
@@ -15,6 +15,22 @@ var paddleX = (canvas.width-paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
 
+var brickRowCount = 2;
+var brickColumnCount = 12;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0 };
+    }
+}
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -52,18 +68,38 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#";
+            ctx.fill();
+            ctx.closePath();
+            
+        }
+    }
+}
+
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     drawPaddle();
-    
+    drawBricks();   
    
     
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
+        ctx.fillStyle = getRandomColor();
     }
     if(y + dy < ballRadius) {
         dy = -dy;
+        ctx.fillStyle = getRandomColor();
     }
     else if(y + dy > canvas.height-ballRadius) {
         if(x > paddleX && x < paddleX + paddleWidth) {
