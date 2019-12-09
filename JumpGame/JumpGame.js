5e497e1a4083a73d90f5f0c6a89f1a128f2e3f53
jordybushoff <47558264+jordybushoff@users.noreply.github.com>
2019-12-09 20:47:07 +0100

    update JumpGame
    proberen de bal in het canvas te plaatsen en goed te scalen, ik ga hier nog verder aan werken want hij is nog niet correct.

--- JumpGame/JumpGame.js
+++ JumpGame/JumpGame.js
@@ -2,5 +2,23 @@ window.onload = function() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     var img = document.getElementById("andereball");
-   ctx.drawImage(img, 10, 10);
+    ctx.drawImage(img, 0, 0);
+    ctx.width = this.naturalWidth;
+    ctx.height = this.naturalHeight;
+  
 };
+
+function drawImageActualSize() {
+  // Use the intrinsic size of image in CSS pixels for the canvas element
+  ctx.width = this.naturalWidth;
+  ctx.height = this.naturalHeight;
+
+  // Will draw the image as 300x227, ignoring the custom size of 60x45
+  // given in the constructor
+  ctx.drawImage(this, 0, 0);
+
+  // To use the custom size we'll have to specify the scale parameters 
+  // using the element's width and height properties - lets draw one 
+  // on top in the corner:
+  ctx.drawImage(this, 0, 0, this.width, this.height);
+}
